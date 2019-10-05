828daa944eb03020c96f091576805b15dfbc60f1
Ana <anamorales.dev@gmail.com>
2019-10-05 18:14:55 -0400

    Moved firebase config to environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,25 @@
 import firebase from 'firebase';
 
+// apiKey: "AIzaSyDYZS3jKKLuglNpC9dcc6-iEdvUItaHi0A",
+    // authDomain: "volunteer-center.firebaseapp.com",
+    // databaseURL: "https://volunteer-center.firebaseio.com",
+    // projectId: "volunteer-center",
+    // storageBucket: "",
+    // messagingSenderId: "893009596874",
+    // appId: "1:893009596874:web:84684da9c2ec072d5eff33"
+
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDYZS3jKKLuglNpC9dcc6-iEdvUItaHi0A",
-    authDomain: "volunteer-center.firebaseapp.com",
-    databaseURL: "https://volunteer-center.firebaseio.com",
-    projectId: "volunteer-center",
-    storageBucket: "",
-    messagingSenderId: "893009596874",
-    appId: "1:893009596874:web:84684da9c2ec072d5eff33"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_ID
 };
+
+console.log(firebaseConfig);
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
