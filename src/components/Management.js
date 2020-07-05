b6da5a1fdaf683f15db71a201ecf5e5d767fa0c7
Ana <anamorales.dev@gmail.com>
2020-07-04 22:54:23 -0400

    Refactored Management component to use react useState hook

--- src/components/Management.js
+++ src/components/Management.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
 import DatePicker from "react-datepicker";
 import { Category } from "./Category";
@@ -8,67 +8,69 @@ import "react-datepicker/dist/react-datepicker-cssmodules.css";
 import "../partials/_main.scss";
 
 export const Management = props => {
-  const dbRef = props.app.dbRef.child("posting"),
-    appState = props.app.state,
-    app = props.app;
+  const dbRef = props.app.dbRef.child("posting");
+  const appState = props.app.state;
+  const app = props.app;
+
+  const [organization, setOrganization] = useState('');
+  const [address, setAddress] = useState('');
+  const [city, setCity] = useState('Toronto');
+  const [stateProvince, setStateProvince] = useState('ON');
+  const [phone, setPhone] = useState('');
+  const [website, setWebsite] = useState('');
+  const [email, setEmail] = useState('');
+  const [category, setCategory] = useState('');
+  const [role, setRole] = useState('');
+  const [roleDescription, setRoleDescription] = useState('');
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
 
   // To automatically set the focus to the first input field after submit
   const organizationInput = React.createRef();
 
-  const handleSubmit = (event) => {
+  const handleSubmit = event => {
     event.preventDefault();
 
     // Validate date range. Do not continue if there's an error.
     if (appState.end_date < appState.start_date) {
-      dateRangeError();
-      return;
+      return dateRangeError();
     }
 
     // The dbRef push property will be able to return the object key but it needs to be assigned to variable first before setting the state
     const newPostRef = dbRef.push();
     newPostRef.set({
       id: newPostRef.key,
-      organization: appState.organization,
-      address: appState.address,
-      state: "ON",
-      city: "Toronto",
-      phone: appState.phone.replace(/-/g, ""),
-      website: appState.website,
-      email: appState.email,
-      category: appState.category,
-      role: appState.role,
-      role_description: appState.role_description,
-      start_date: appState.start_date.toLocaleDateString(),
-      end_date: appState.end_date.toLocaleDateString(),
-      created: appState.created,
+      organization,
+      address,
+      state: stateProvince,
+      city,
+      phone: phone.replace(/-/g, ""),
+      website,
+      email,
+      category,
+      role,
+      role_description: roleDescription,
+      start_date: startDate.toLocaleDateString(),
+      end_date: endDate.toLocaleDateString(),
+      created: Date.now()
     });
 
     // Clear the fields after submit
-    app.setState({
-      organization: "",
-      address: "",
-      state: "ON",
-      city: "Toronto",
-      phone: "",
-      website: "",
-      email: "",
-      category: "",
-      role: "",
-      role_description: "",
-      start_date: new Date(),
-      end_date: new Date(),
-      created: Date.now(),
-    });
+    setOrganization('');
+    setAddress('');
+    setStateProvince('ON');
+    setCity("Toronto");
+    setPhone('');
+    setWebsite('');
+    setEmail('');
+    setCategory('');
+    setRole('');
+    setRoleDescription('');
+    setStartDate(new Date());
+    setEndDate(new Date());
 
     organizationInput.current.focus();
-
     saveSuccessful();
-  };
-
-  const handleChange = (event) => {
-    app.setState({
-      [event.target.name]: event.target.value,
-    });
   };
 
   return (
@@ -77,153 +79,165 @@ export const Management = props => {
         <fieldset>
           <legend>Organization Information</legend>
           <div className="fieldsContainer">
-            <label htmlFor="organization">
+            <label>
               Name <span>*</span>
-            </label>
-            <input
-              id="organization"
-              type="text"
-              name="organization"
-              onChange={handleChange}
-              value={appState.organization}
-              placeholder="Animal Shelter"
-              size="30"
-              ref={organizationInput}
-              required
-            />
+              <input
+                id="organization"
+                type="text"
+                name="organization"
+                onChange={e => setOrganization(e.target.value)}
+                value={organization}
+                placeholder="Animal Shelter"
+                size="30"
+                ref={organizationInput}
+                required
+              />
 
-            <label htmlFor="address">
+            </label>
+
+            <label>
               Address <span>*</span>
+              <input
+                id="address"
+                type="text"
+                name="address"
+                onChange={e => setAddress(e.target.value)}
+                value={address}
+                placeholder="123 Main Street"
+                required
+              />
             </label>
-            <input
-              id="address"
-              type="text"
-              name="address"
-              onChange={handleChange}
-              value={appState.address}
-              placeholder="123 Main Street"
-              required
-            />
 
-            <label htmlFor="city">City</label>
-            <input
-              id="city"
-              type="text"
-              name="city"
-              onChange={handleChange}
-              value={appState.city}
-              disabled
-            />
+            <label>
+              City
+              <input
+                id="city"
+                type="text"
+                name="city"
+                onChange={e => setCity(e.target.value)}
+                value={city}
+                disabled
+              />
+            </label>
 
             <label htmlFor="state">State</label>
             <input
               id="state"
               type="text"
               name="state"
-              onChange={handleChange}
-              value={appState.state}
+              onChange={e => setStateProvince(e.target.value)}
+              value={stateProvince}
               disabled
             />
 
-            <label htmlFor="phone">
+            <label>
               Phone <span>*</span>
+              <input
+                id="phone"
+                type="tel"
+                name="phone"
+                pattern="[\d]{3}-?[\d]{3}-?[\d]{4}"
+                onChange={e => setPhone(e.target.value)}
+                value={phone}
+                placeholder="416-123-456"
+                title="Format: 416-123-456 or 416123456"
+                required
+              />
             </label>
-            <input
-              id="phone"
-              type="tel"
-              name="phone"
-              pattern="[\d]{3}-?[\d]{3}-?[\d]{4}"
-              onChange={handleChange}
-              value={appState.phone}
-              placeholder="416-123-456"
-              title="Format: 416-123-456 or 416123456"
-              required
-            />
 
-            <label htmlFor="website">Website</label>
-            <input
-              id="website"
-              type="url"
-              name="website"
-              onChange={handleChange}
-              value={appState.website}
-              placeholder="https://organization.com"
-            />
+            <label>
+              Website
+              <input
+                id="website"
+                type="url"
+                name="website"
+                onChange={e => setWebsite(e.target.value)}
+                value={website}
+                placeholder="https://organization.com"
+              />
 
-            <label htmlFor="email">Email</label>
-            <input
-              id="email"
-              type="email"
-              name="email"
-              onChange={handleChange}
-              value={appState.email}
-              size="30"
-              placeholder="johndoe@domain.com"
-            />
+            </label>
+            
+            <label>
+              Email
+              <input
+                id="email"
+                type="email"
+                name="email"
+                onChange={e => setEmail(e.target.email)}
+                value={email}
+                size="30"
+                placeholder="johndoe@domain.com"
+              />
+            </label>
           </div>
         </fieldset>
 
         <fieldset>
           <legend>About the Role</legend>
           <div className="fieldsContainer">
-            <label htmlFor="category">
+            <label>
               Category <span>*</span>
+              <Category
+                name="category"
+                id="category"
+                app={app}
+                onChange={e => setCategory(e.target.value)}
+                value={category}
+                required="required"
+                defaultText="Select Category"
+              />
             </label>
-            <Category
-              name="category"
-              id="category"
-              app={app}
-              onChange={handleChange}
-              value={appState.category}
-              required="required"
-              defaultText="Select Category"
-            />
 
-            <label htmlFor="role">
+            <label>
               Role <span>*</span>
+              <input
+                id="role"
+                type="text"
+                name="role"
+                onChange={e => setRole(e.target.value)}
+                value={role}
+                placeholder="Dog Walker"
+                required
+              />
             </label>
-            <input
-              id="role"
-              type="text"
-              name="role"
-              onChange={handleChange}
-              value={appState.role}
-              placeholder="Dog Walker"
-              required
-            />
-
-            <label htmlFor="roleDescription">
+            
+            <label>
               Description <span>*</span>
+              <textarea
+                className="textArea"
+                maxLength="500"
+                id="roleDescription"
+                type="text"
+                name="role_description"
+                onChange={e => setRoleDescription(e.target.value)}
+                value={roleDescription}
+                placeholder="What is the role about? How to apply? (Maximum of 500 characters)"
+                required
+              />
             </label>
-            <textarea
-              className="textArea"
-              maxLength="500"
-              id="roleDescription"
-              type="text"
-              name="role_description"
-              onChange={handleChange}
-              value={appState.role_description}
-              placeholder="What is the role about? How to apply? (Maximum of 500 characters)"
-              required
-            />
-
+            
             <div className="startEndDateContainer">
               <div className="dateContainer">
-                <label htmlFor="startDate">Start Date</label>
-                <DatePicker
-                  selected={appState.start_date}
-                  onChange={(e) => app.setState({ start_date: e })}
-                  minDate={new Date()}
-                />
+                <label>
+                  Start Date
+                  <DatePicker
+                    selected={startDate}
+                    onChange={e => setStartDate(e)}
+                    minDate={new Date()}
+                  />
+                </label>
               </div>
 
               <div className="dateContainer">
-                <label htmlFor="endDate">End Date</label>
-                <DatePicker
-                  selected={appState.end_date}
-                  onChange={(e) => app.setState({ end_date: e })}
-                  minDate={appState.start_date}
-                />
+                <label>
+                  End Date
+                  <DatePicker
+                    selected={endDate}
+                    onChange={e => setEndDate(e)}
+                    minDate={appState.start_date}
+                  />
+                </label>
               </div>
             </div>
           </div>
