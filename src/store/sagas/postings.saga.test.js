73883486e978c8e1eeb974663bc5dea1305b2cf3
Ana <anamorales.dev@gmail.com>
2020-08-23 17:29:42 -0400

    Removed unnecssary imports

--- src/store/sagas/postings.saga.test.js
+++ src/store/sagas/postings.saga.test.js
@@ -1,43 +1,41 @@
-import { put, call , take, actionChannel} from 'redux-saga/effects';
+import { put} from 'redux-saga/effects';
 import {
   fetchPostingsAsync,
   addNewPosting
 } from './index';
 import {
   requestPostingsStart,
-  requestPostingsSuccess,
+  // requestPostingsSuccess,
   requestPostingsError,
-  addPostingSuccess
 } from '../actions/postingActions';
 import {
-  createEventChannel,
+  // createEventChannel,
   firebasePush,
   firebaseSetData
 } from '../../services/firebase-data.service'
-import { channel } from 'redux-saga';
 
 jest.mock('../../services/firebase-data.service')
 
 describe('Postings saga', () => {
   describe('fetchPostingsAsync', () => {
-    const payload = [
-      {
-        address: "XMain Street",
-        category: "community",
-        city: "Toronto",
-        created: 1596814662926,
-        email: "",
-        end_date: "12/31/2020",
-        id: "-ME8ZX4A3UqwBvruUW2C",
-        organization: "JS Community Center",
-        phone: "4162221111",
-        role: "Frontend Developer",
-        role_description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo ab itaque hic dolorem. Sit unde eos dolorem, voluptatum labore ut reprehenderit totam! Laudantium culpa nemo excepturi repellat facere deserunt atque.",
-        start_date: "8/7/2020",
-        state: "ON",
-        website: "https://testorg.com"
-      }
-    ]
+    // const payload = [
+    //   {
+    //     address: "XMain Street",
+    //     category: "community",
+    //     city: "Toronto",
+    //     created: 1596814662926,
+    //     email: "",
+    //     end_date: "12/31/2020",
+    //     id: "-ME8ZX4A3UqwBvruUW2C",
+    //     organization: "JS Community Center",
+    //     phone: "4162221111",
+    //     role: "Frontend Developer",
+    //     role_description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo ab itaque hic dolorem. Sit unde eos dolorem, voluptatum labore ut reprehenderit totam! Laudantium culpa nemo excepturi repellat facere deserunt atque.",
+    //     start_date: "8/7/2020",
+    //     state: "ON",
+    //     website: "https://testorg.com"
+    //   }
+    // ]
 
     it('should return postings', () => {
       const generator = fetchPostingsAsync({ category: 'community' });
